8b4b61abdd94192c52a4829c3494c15c1fd2885e
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-02-22 16:38:40 +0900

    fix: お知らせを取得する条件を修正した

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
+use App\Enums\NoticeState;
 use App\Models\Notice;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/home');
+
 Route::get('/home', function () {
-    $notices = Notice::where('published_on', '<=', now())->get();
+    $notices = Notice::where('published_on', '<=', now())
+                     ->where('state', NoticeState::Published)
+                     ->get();
     return view('home', compact('notices'));
 });
