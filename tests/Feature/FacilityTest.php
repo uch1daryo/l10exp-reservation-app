a227e7982cbe8b1a941baeb2b1b52eee5d86304c
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-02-27 15:28:49 +0900

    test: 設備ごとのカレンダーを表示しているかテストするように修正した

--- tests/Feature/FacilityTest.php
+++ tests/Feature/FacilityTest.php
@@ -2,13 +2,21 @@
 
 namespace Tests\Feature;
 
+use App\Models\Facility;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class FacilityTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testCanSeeCalendarThatSpecifiedFacility(): void
     {
-        $response = $this->get('/facilities/1');
-        $response->assertSee('<div id="calendar"></div>', false);
+        $facility = new Facility();
+        $facility->name = 'A1会議室';
+        $facility->save();
+        $response = $this->get('/facilities/' . $facility->id);
+        $response->assertSee('<div id="calendar"></div>', false)
+                 ->assertSeeText($facility->name);
     }
 }
