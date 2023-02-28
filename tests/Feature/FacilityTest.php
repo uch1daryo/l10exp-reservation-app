5f37a78d6ad52ecf0a8d7014a75af717839764a9
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-02-28 11:55:38 +0900

    test: 指定した設備の予約情報を取得できるかのテストを追加した

--- tests/Feature/FacilityTest.php
+++ tests/Feature/FacilityTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\Facility;
+use App\Models\Reservation;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class FacilityTest extends TestCase
@@ -24,5 +26,38 @@ class FacilityTest extends TestCase
     {
         $response = $this->get('/facilities/1234567890');
         $response->assertStatus(404);
+    }
+
+    public function testCanGetReservationThatSpecifiedFacility(): void
+    {
+        $facility = new Facility();
+        $facility->name = 'A1会議室';
+        $facility->save();
+
+        $reservation = new Reservation();
+        $reservation->facility_id = $facility->id;
+        $reservation->user_name = '山田 太郎';
+        $reservation->user_email = 'yamadataro@example.com';
+        $reservation->purpose = '打ち合わせ';
+        $reservation->start_at = '2023-03-01 09:30:00';
+        $reservation->end_at = '2023-03-01 11:00:00';
+        $reservation->cancel_code = hash('sha256', spl_object_hash($reservation));
+        $reservation->save();
+
+        $response = $this->getJson('/api/facilities/' . $facility->id);
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->has(1)
+                ->first(fn (AssertableJson $json) =>
+                    $json->where('id', $reservation->id)
+                        ->where('facility_id', $reservation->facility_id)
+                        ->where('user_name', $reservation->user_name)
+                        ->where('user_email', $reservation->user_email)
+                        ->where('purpose', $reservation->purpose)
+                        ->where('start_at', $reservation->start_at)
+                        ->where('end_at', $reservation->end_at)
+                        ->where('cancel_code', $reservation->cancel_code)
+                        ->etc()
+                )
+        );
     }
 }
