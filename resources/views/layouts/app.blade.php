a1041a516a9c209882c62ec12c51bf125714bd2d
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-03-01 09:42:47 +0900

    feat: ナビゲーションバーに設備へのリンクを追加した

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,23 @@
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-      <span class="navbar-brand">{{ config('app.name') }}</span>
+      <a class="navbar-brand" href="/home">{{ config('app.name') }}</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="/home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/facilities/1">A1会議室</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/facilities/2">A2会議室</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
   <main class="container">
