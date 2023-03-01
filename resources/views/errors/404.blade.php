337cc60ca872297bde02cd64904c38d067381774
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-03-01 09:42:19 +0900

    feat: 404 Not Foundのカスタムページを追加した

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', '404 Not Found')
+
+@section('content')
+<div class="mt-4">
+  <div class="alert alert-danger" role="alert">
+    404 Not Found
+  </div>
+</div>
+@endsection
