73007a161eec06f97eab76e5173a2637713f969a
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-03-02 15:31:35 +0900

    refactor: 設備予約関連を担当するコントローラーをリネームした

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
-class FacilityReservationController extends Controller
+class ReservationController extends Controller
 {
     public function index(string $id): View
     {
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Http\Controllers\FacilityReservationController;
+use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/home');
 Route::get('/home', [HomeController::class, 'index']);
 
-Route::controller(FacilityReservationController::class)->group(function () {
+Route::controller(ReservationController::class)->group(function () {
     Route::get('/facilities/{id}/reservations', 'index');
     Route::get('/facilities/{id}/reservations/create', 'create');
     Route::post('/facilities/{id}/reservations', 'store');
