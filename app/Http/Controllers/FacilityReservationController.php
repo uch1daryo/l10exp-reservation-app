b82ecf856108d70f97ddf5bf620379529f91776c
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-03-02 14:32:53 +0900

    refactor: 設備予約をFacilityReservationControllerに集約した

---
+++ app/Http/Controllers/FacilityReservationController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Facility;
+use App\Models\Reservation;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class FacilityReservationController extends Controller
+{
+    public function index(string $id): View
+    {
+        $facility = Facility::findOrFail($id);
+        return view('facilities.index', compact('facility'));
+    }
+
+    public function create(string $id): View
+    {
+        $facility = Facility::findOrFail($id);
+        return view('facilities.create', compact('facility'));
+    }
+
+    public function store(Request $request, string $id): RedirectResponse
+    {
+        $reservation = new Reservation();
+        $reservation->facility_id = $id;
+        $reservation->user_name = $request->input('user_name');
+        $reservation->user_email = $request->input('user_email');
+        $reservation->purpose = $request->input('purpose');
+        $reservation->start_at = $request->input('start_at');
+        $reservation->end_at = $request->input('end_at');
+        $reservation->note = $request->input('note');
+        $reservation->cancel_code = hash('sha256', spl_object_hash($reservation));;
+        $reservation->save();
+
+        return redirect('/facilities/' . $id . '/reservations')->with(
+            'status',
+            '登録が完了しました'
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,14 @@
 <?php
 
+use App\Http\Controllers\FacilityReservationController;
 use App\Http\Controllers\HomeController;
-use App\Models\Facility;
-use App\Models\Reservation;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/home');
 Route::get('/home', [HomeController::class, 'index']);
 
-Route::get('/facilities/{facility_id}/reservations', function ($facility_id) {
-    $facility = Facility::findOrFail($facility_id);
-    return view('facilities.index', compact('facility'));
-})->whereNumber('facility_id');
-
-Route::get('/facilities/{facility_id}/reservations/create', function ($facility_id) {
-    $facility = Facility::findOrFail($facility_id);
-    return view('facilities.create', compact('facility'));
-})->whereNumber('facility_id');
-
-Route::post('/facilities/{facility_id}/reservations', function ($facility_id, Request $request) {
-    $reservation = new Reservation();
-    $reservation->facility_id = $facility_id;
-    $reservation->user_name = $request->input('user_name');
-    $reservation->user_email = $request->input('user_email');
-    $reservation->purpose = $request->input('purpose');
-    $reservation->start_at = $request->input('start_at');
-    $reservation->end_at = $request->input('end_at');
-    $reservation->note = $request->input('note');
-    $reservation->cancel_code = hash('sha256', spl_object_hash($reservation));;
-    $reservation->save();
-
-    return redirect('/facilities/' . $facility_id . '/reservations')->with(
-        'status',
-        '登録が完了しました'
-    );
-})->whereNumber('facility_id');
+Route::controller(FacilityReservationController::class)->group(function () {
+    Route::get('/facilities/{id}/reservations', 'index');
+    Route::get('/facilities/{id}/reservations/create', 'create');
+    Route::post('/facilities/{id}/reservations', 'store');
+})->whereNumber('id');
