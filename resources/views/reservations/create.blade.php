062f22d318d48d2d91b660c3c8938092e5a16d0b
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-03-07 13:05:37 +0900

    feat: 予約の登録における入力エラーを表示する機能を追加した

--- resources/views/reservations/create.blade.php
+++ resources/views/reservations/create.blade.php
@@ -4,6 +4,15 @@
 
 @section('content')
 <div class="mt-4">
+  @if ($errors->any())
+  <div class="alert alert-danger" role="alert">
+    <ul>
+      @foreach ($errors->all() as $error)
+      <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
   <form method="POST" action="/facilities/{{ $facility->id }}/reservations">
     @csrf
     <div class="mb-3">
