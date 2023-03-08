ec409f62e5dfb1b95a9d2590644ef90ea372df60
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-03-08 10:04:40 +0900

    feat: 400 Bad RequestのBladeを追加した

---
+++ resources/views/errors/400.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', '400 Bad Request')
+
+@section('content')
+<div class="mt-4">
+  <div class="alert alert-danger" role="alert">
+    400 Bad Request
+  </div>
+</div>
+@endsection
