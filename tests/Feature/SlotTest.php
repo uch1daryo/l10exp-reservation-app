69ed077a6c5562b8b2bf68f8d0131c250969b577
Uchida Ryo <uchidaryo.gm@gmail.com>
2023-03-13 08:34:48 +0900

    test: 予約の時間帯が早すぎたり遅すぎたりすると例外を投げるテストを追加した

--- tests/Feature/SlotTest.php
+++ tests/Feature/SlotTest.php
@@ -2,12 +2,25 @@
 
 namespace Tests\Feature;
 
+use App\Exceptions\InvalidTimeBookingException;
+use App\Models\Facility;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class SlotTest extends TestCase
 {
     use RefreshDatabase;
+
+    private Facility $facility;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->facility = new Facility();
+        $this->facility->name = 'A1会議室';
+        $this->facility->save();
+    }
 
     /**
      * @test
@@ -19,5 +32,47 @@ class SlotTest extends TestCase
 
         $this->artisan('slot:add ' . $year)->assertExitCode(0);
         $this->assertDatabaseCount('slots', $numberOfDaysInYear);
+    }
+
+    /**
+     * @test
+     */
+    public function 早すぎる時間帯に予約を登録しようとすると例外が投げられる(): void
+    {
+        $this->withoutExceptionHandling();
+        $this->expectException(InvalidTimeBookingException::class);
+
+        $this->artisan('slot:add ' . 2023)->assertExitCode(0);
+
+        $reservation = [
+            'user_name' => '鈴木 花子',
+            'user_email' => 'suzukihanako@example.com',
+            'purpose' => '期末試験',
+            'start_at' => '2023-03-01 05:00:00',
+            'end_at' => '2023-03-01 06:00:00',
+            'note' => '応用数学（佐藤先生）',
+        ];
+        $response = $this->post('/facilities/' . $this->facility->id . '/reservations', $reservation);
+    }
+
+    /**
+     * @test
+     */
+    public function 遅すぎる時間帯に予約を登録しようとしても例外が投げられる(): void
+    {
+        $this->withoutExceptionHandling();
+        $this->expectException(InvalidTimeBookingException::class);
+
+        $this->artisan('slot:add ' . 2023)->assertExitCode(0);
+
+        $reservation = [
+            'user_name' => '鈴木 花子',
+            'user_email' => 'suzukihanako@example.com',
+            'purpose' => '期末試験',
+            'start_at' => '2023-03-01 21:00:00',
+            'end_at' => '2023-03-01 22:00:00',
+            'note' => '応用数学（佐藤先生）',
+        ];
+        $response = $this->post('/facilities/' . $this->facility->id . '/reservations', $reservation);
     }
 }
